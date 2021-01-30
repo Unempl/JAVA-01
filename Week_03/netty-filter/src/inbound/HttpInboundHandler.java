35bf08472e1194cc45a47dc108ca45b95174320c
WindyZoeng <5489501+WindyZoeng@user.noreply.gitee.com>
2021-01-31 07:55:21 +0800

    commit for netty-filter

---
+++ Week_03/netty-filter/src/inbound/HttpInboundHandler.java
@@ -0,0 +1,110 @@
+package inbound;
+
+import filter.HeaderHttpRequestFilter;
+import filter.HttpRequestFilter;
+import router.HttpEndpointRouter;
+import router.RandomHttpEndpointRouter;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpUtil;
+import io.netty.util.ReferenceCountUtil;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.List;
+
+import static io.netty.handler.codec.http.HttpHeaderNames.CONNECTION;
+import static io.netty.handler.codec.http.HttpHeaderValues.KEEP_ALIVE;
+import static io.netty.handler.codec.http.HttpResponseStatus.NO_CONTENT;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
+
+public class HttpInboundHandler extends ChannelInboundHandlerAdapter {
+
+    private static Logger logger = LoggerFactory.getLogger(HttpInboundHandler.class);
+    private final List<String> proxyServer;
+    private httpclient4.HttpOutboundHandler handler;
+    private HttpRequestFilter filter = new HeaderHttpRequestFilter();
+    HttpEndpointRouter router = new RandomHttpEndpointRouter();
+
+    public HttpInboundHandler(List<String> proxyServer) {
+        this.proxyServer = proxyServer;
+        this.handler = new httpclient4.HttpOutboundHandler(this.proxyServer);
+    }
+    
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.flush();
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        try {
+            //logger.info("channelRead流量接口请求开始，时间为{}", startTime);
+            FullHttpRequest fullRequest = (FullHttpRequest) msg;
+            String uri = fullRequest.uri();
+            //logger.info("接收到的请求url为{}", uri);
+            if (uri.contains("/test")) {
+                handlerTest(fullRequest, ctx);
+            }
+    
+            handler.handle(fullRequest, ctx, filter);
+    
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
+    }
+
+    private void handlerTest(FullHttpRequest fullRequest, ChannelHandlerContext ctx) {
+        FullHttpResponse response = null;
+        try {
+            String value = getRequestStr(router.route(this.proxyServer));
+            response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer(value.getBytes("UTF-8")));
+            response.headers().set("Content-Type", "application/json");
+            response.headers().setInt("Content-Length", response.content().readableBytes());
+        } catch (Exception e) {
+            logger.error("处理测试接口出错", e);
+            response = new DefaultFullHttpResponse(HTTP_1_1, NO_CONTENT);
+        } finally {
+            if (fullRequest != null) {
+                if (!HttpUtil.isKeepAlive(fullRequest)) {
+                    ctx.write(response).addListener(ChannelFutureListener.CLOSE);
+                } else {
+                    response.headers().set(CONNECTION, KEEP_ALIVE);
+                    ctx.write(response);
+                }
+            }
+        }
+    }
+
+    private String getRequestStr(String gatewayUrl) throws IOException {
+        //创建okhttpclient对象
+        OkHttpClient client = new OkHttpClient().newBuilder().build();
+        Request request = new Request.Builder().url(gatewayUrl).get().build();
+        Response response = client.newCall(request).execute();
+        if (response.isSuccessful()) {
+            ResponseBody responseBody = response.body();
+            return responseBody.string();
+        }
+        return null;
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+}
